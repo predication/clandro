d1634d7a1e45cf46c09d88694719c6e97e79f804
predication <ricketts30@yahoo.com[user]>
2013-03-21 22:38:13 +0000

    now parsing WHITESPACE

--- DEV/Predication.Clandro/Token.cs
+++ DEV/Predication.Clandro/Token.cs
@@ -9,10 +9,19 @@ namespace Predication.Clandro
     /// </summary>
     public class Token
     {
-        public int ColumnNumber { get; private set; }
+        public int LinePosition { get; private set; }
         public int LineNumber { get; private set; }
         public string Text { get; private set; }
         public string Type { get; private set; }
-        public bool IsWhiteSpace { get; private set; }
+        public bool IsWhitespace { get; private set; }
+
+        public Token(int lineNumber, int linePosition, string text, string type, bool isWhitespace)
+        {
+            LineNumber = LineNumber;
+            LinePosition = linePosition;
+            Text = text;
+            Type = type;
+            IsWhitespace = isWhitespace;
+        }
     }
 }
